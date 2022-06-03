a758e55bb19613d5f33160d2cc5dc2608bcb2064
Federico Reyes Gomez <frg100@gmail.com>
2022-06-03 06:54:58 +0000

    tight

--- plot_results.py
+++ plot_results.py
@@ -1,4 +1,4 @@
 import utils
 
 f = utils.plot_results([("BERT_word_norm", "FB15k-237"),('BERT', 'FB15k-237'), ('GPT2_gpt2', 'FB15k-237'), ('GPT2_gpt2-large', 'FB15k-237')], 25, 1000)
-f.savefig('results_1_long.png')
+f.savefig('results_1_long.png', bbox_inches="tight")
